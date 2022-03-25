17dce087bf2ce996491c22c3deb0e700256e95d9
Michael <michael.c.montgomery@gmail.com>
2022-03-25 16:32:38 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const arrayLetters = word.split('')
+  const reverseLetters = arrayLetters.reverse()
+  const reverseWord = reverseLetters.join('')
+  if (word === reverseWord) {
+    return true
+  }
+  else return false
 }
 
 /* 
   Add your pseudocode here
+  .split method
+  .reverse method
+  .join method
+  if word === reverseWord then its a palindrome, else no palindrome 
 */
 
 /*
   Add written explanation of your solution here
+  the .split method converts the string into an array of individual characters
+  the .reverse method reverses the positions of the array
+  the .join method joins all the elements of the array into a string
+  the if/else checks to see if the new reverse string is the same as the original, if it is returns the word palindrome!
 */
 
 // You can run `node index.js` to view these console logs
@@ -16,10 +31,14 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("=>", isPalindrome('mom'));
+
+  console.log('=>', isPalindrome('dad'));
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log('=>', isPalindrome('nessie'));
 }
 
 module.exports = isPalindrome;
